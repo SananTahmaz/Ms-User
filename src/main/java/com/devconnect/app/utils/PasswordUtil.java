5b6a37921c6bb7087e786c995f504cb7d7b5b47f
sanant <sanant@azerconnect.az>
2026-02-15 01:19:53 +0400

    Created PasswordUtil for hashing user's raw password

---
+++ src/main/java/com/devconnect/app/utils/PasswordUtil.java
@@ -0,0 +1,9 @@
+package com.devconnect.app.utils;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordUtil {
+    public static String hash(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+}
