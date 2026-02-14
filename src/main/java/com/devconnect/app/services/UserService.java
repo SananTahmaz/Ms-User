bd64181025cb749c9328f7c5d80d3a10187d0fc0
sanant <sanant@azerconnect.az>
2026-02-14 23:49:48 +0400

    Created repository and service related with User entity

---
+++ src/main/java/com/devconnect/app/services/UserService.java
@@ -0,0 +1,4 @@
+package com.devconnect.app.services;
+
+public interface UserService {
+}
---
+++ src/main/java/com/devconnect/app/services/implementations/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.devconnect.app.services.implementations;
+
+import com.devconnect.app.services.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
